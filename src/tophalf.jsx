a969fb982d5e29410e63127363210c861f058dab
Brandon Marquez <brandonmarquez@mac.mynetworksettings.com>
2025-10-22 15:22:51 -0400

    Working

--- src/tophalf.jsx
+++ src/tophalf.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
+import Base from "./img/base.png";
 export default function Tophalf() {
   const [locationIcon, setLocationIcon] = useState(false);
   const [userLocation, setUserLocation] = useState({ lat: null, log: null });
@@ -31,7 +32,7 @@ export default function Tophalf() {
     try {
       const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&key=${
         import.meta.env.VITE_WEATHER_KEY
-      }&include=current`;
+      }&include=current,days`;
       const respone = await fetch(url);
       const data = await respone.json();
       console.log(data);
@@ -55,22 +56,39 @@ export default function Tophalf() {
     }
   };
 
+  const formatdate = (olddate) =>{
+    const newdate = olddate.split("-");
+    return `${newdate[1]}-${newdate[2]}-${newdate[0]}`
+  }
   return (
-    <div className={styles.Tophalf}>
+    <div
+      className={styles.Tophalf}
+      style={{
+        background: `url(${Base}),linear-gradient(to bottom,navy,aqua)`,
+        backgroundSize: "cover,cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div className={styles.navBar}>
-        <div
-          className={styles.currentLocation}
-          onMouseEnter={() => setLocationIcon(true)}
-          onMouseLeave={() => setLocationIcon(false)}
-        >
-          <FontAwesomeIcon
-            icon={faLocationDot}
-            size="2x"
-            color="white"
-            bounce={locationIcon}
-            style={{ paddingTop: "10px" }}
-          ></FontAwesomeIcon>
-          <h1>{area}</h1>
+        <div className={styles.currentLocation}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onMouseEnter={() => setLocationIcon(true)}
+            onMouseLeave={() => setLocationIcon(false)}
+          >
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              size="2x"
+              color="white"
+              bounce={locationIcon}
+              style={{ paddingTop: "10px" }}
+            ></FontAwesomeIcon>
+            <h1 style={{ color: "white" }}>{area}</h1>
+          </div>
         </div>
         <input
           type="text"
@@ -78,6 +96,9 @@ export default function Tophalf() {
           placeholder="Search"
         ></input>
         <div className={styles.map}>
+          <h1 className={styles.date} style={{color:"white"}}>
+            {userData.days && userData.days[0]? formatdate(userData.days[0].datetime) :"Loading"}
+          </h1>
           <button>
             <FontAwesomeIcon
               icon={faMapLocationDot}
@@ -89,8 +110,41 @@ export default function Tophalf() {
       </div>
       <div className={styles.content}>
         <div className={styles.left}>
-          <div className={styles.weatherIcon}></div>
-          <div className={styles.currentInfo}></div>
+          <div className={styles.weatherIcon}>
+            <div className={styles.chanceRainWind}></div>
+            <img
+              src="https://www.clipartqueen.com/image-files/happy-baby-sun.png"
+              style={{ width: "50%" }}
+            ></img>
+          </div>
+          <div className={styles.currentInfo}>
+            <div className={styles.temp}>
+              <h1 style={{ fontSize: "4rem", fontWeight: "bold" }}>
+                {userData.currentConditions
+                  ? ((userData.currentConditions.temp * 9) / 5 + 32).toFixed(
+                      0
+                    ) + "°"
+                  : "Loading..."}
+              </h1>
+              <div className={styles.lowHigh}>
+                <h1>
+                  {userData.days && userData.days[0]
+                    ? "L: " +
+                      ((userData.days[0].tempmin * 9) / 5 + 32).toFixed(0) +
+                      "°"
+                    : "Loading"}
+                </h1>
+                <h1>
+                  {userData.days && userData.days[0]
+                    ? "H: " +
+                      ((userData.days[0].tempmax * 9) / 5 + 32).toFixed(0) +
+                      "°"
+                    : "Loading"}
+                </h1>
+              </div>
+            </div>
+            <div className={styles.otherInfo}></div>
+          </div>
         </div>
         <div className={styles.right}></div>
       </div>
